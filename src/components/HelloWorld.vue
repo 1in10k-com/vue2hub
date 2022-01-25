10ef6d573c6ed946ae0457483f8ff9494ac23420
playdog-io <cqdxcs3@163.com>
2022-01-25 09:39:57 +0800

    .

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,181 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+    <div class="line red">
+      <div style="background-color: #000000">000000</div>
+      <div style="background-color: #000040">000040</div>
+      <div style="background-color: #000080">000080</div>
+      <div style="background-color: #0000c0">0000c0</div>
+      <div style="background-color: #0000ff">0000ff</div>
+      <div style="background-color: #004000">004000</div>
+      <div style="background-color: #004040">004040</div>
+      <div style="background-color: #004080">004080</div>
+      <div style="background-color: #0040c0">0040c0</div>
+      <div style="background-color: #0040f0">0040f0</div>
+      <div style="background-color: #008000">008000</div>
+      <div style="background-color: #008040">008040</div>
+      <div style="background-color: #008080">008080</div>
+      <div style="background-color: #0080c0">0080c0</div>
+      <div style="background-color: #0080f0">0080f0</div>
+      <div style="background-color: #00c000">00c000</div>
+      <div style="background-color: #00c040">00c040</div>
+      <div style="background-color: #00c080">00c080</div>
+      <div style="background-color: #00c0c0">00c0c0</div>
+      <div style="background-color: #00c0f0">00c0f0</div>
+      <div style="background-color: #00f000">00f000</div>
+      <div style="background-color: #00f040">00f040</div>
+      <div style="background-color: #00f080">00f080</div>
+      <div style="background-color: #00f0c0">00f0c0</div>
+      <div style="background-color: #00f0f0">00f0f0</div>
+    </div>
+
+    <div class="line red">
+      <div style="background-color: #400000">400000</div>
+      <div style="background-color: #400040">400040</div>
+      <div style="background-color: #400080">400080</div>
+      <div style="background-color: #4000c0">4000c0</div>
+      <div style="background-color: #4000ff">4000ff</div>
+      <div style="background-color: #404000">404000</div>
+      <div style="background-color: #404040">404040</div>
+      <div style="background-color: #404080">404080</div>
+      <div style="background-color: #4040c0">4040c0</div>
+      <div style="background-color: #4040f0">4040f0</div>
+      <div style="background-color: #408000">408000</div>
+      <div style="background-color: #408040">408040</div>
+      <div style="background-color: #408080">408080</div>
+      <div style="background-color: #4080c0">4080c0</div>
+      <div style="background-color: #4080f0">4080f0</div>
+      <div style="background-color: #40c000">40c000</div>
+      <div style="background-color: #40c040">40c040</div>
+      <div style="background-color: #40c080">40c080</div>
+      <div style="background-color: #40c0c0">40c0c0</div>
+      <div style="background-color: #40c0f0">40c0f0</div>
+      <div style="background-color: #40f000">40f000</div>
+      <div style="background-color: #40f040">40f040</div>
+      <div style="background-color: #40f080">40f080</div>
+      <div style="background-color: #40f0c0">40f0c0</div>
+      <div style="background-color: #40f0f0">40f0f0</div>
+    </div>
+
+    <div class="line red">
+      <div style="background-color: #800000">800000</div>
+      <div style="background-color: #800040">800040</div>
+      <div style="background-color: #800080">800080</div>
+      <div style="background-color: #8000c0">8000c0</div>
+      <div style="background-color: #8000ff">8000ff</div>
+      <div style="background-color: #804000">804000</div>
+      <div style="background-color: #804040">804040</div>
+      <div style="background-color: #804080">804080</div>
+      <div style="background-color: #8040c0">8040c0</div>
+      <div style="background-color: #8040f0">8040f0</div>
+      <div style="background-color: #808000">808000</div>
+      <div style="background-color: #808040">808040</div>
+      <div style="background-color: #808080">808080</div>
+      <div style="background-color: #8080c0">8080c0</div>
+      <div style="background-color: #8080f0">8080f0</div>
+      <div style="background-color: #80c000">80c000</div>
+      <div style="background-color: #80c040">80c040</div>
+      <div style="background-color: #80c080">80c080</div>
+      <div style="background-color: #80c0c0">80c0c0</div>
+      <div style="background-color: #80c0f0">80c0f0</div>
+      <div style="background-color: #80f000">80f000</div>
+      <div style="background-color: #80f040">80f040</div>
+      <div style="background-color: #80f080">80f080</div>
+      <div style="background-color: #80f0c0">80f0c0</div>
+      <div style="background-color: #80f0f0">80f0f0</div>
+    </div>
+
+    <div class="line">
+      <div style="background-color: #c00000">c00000</div>
+      <div style="background-color: #c00040">c00040</div>
+      <div style="background-color: #c00080">c00080</div>
+      <div style="background-color: #c000c0">c000c0</div>
+      <div style="background-color: #c000ff">c000ff</div>
+      <div style="background-color: #c04000">c04000</div>
+      <div style="background-color: #c04040">c04040</div>
+      <div style="background-color: #c04080">c04080</div>
+      <div style="background-color: #c040c0">c040c0</div>
+      <div style="background-color: #c040f0">c040f0</div>
+      <div style="background-color: #c08000">c08000</div>
+      <div style="background-color: #c08040">c08040</div>
+      <div style="background-color: #c08080">c08080</div>
+      <div style="background-color: #c080c0">c080c0</div>
+      <div style="background-color: #c080f0">c080f0</div>
+      <div style="background-color: #c0c000">c0c000</div>
+      <div style="background-color: #c0c040">c0c040</div>
+      <div style="background-color: #c0c080">c0c080</div>
+      <div style="background-color: #c0c0c0">c0c0c0</div>
+      <div style="background-color: #c0c0f0">c0c0f0</div>
+      <div style="background-color: #c0f000">c0f000</div>
+      <div style="background-color: #c0f040">c0f040</div>
+      <div style="background-color: #c0f080">c0f080</div>
+      <div style="background-color: #c0f0c0">c0f0c0</div>
+      <div style="background-color: #c0f0f0">c0f0f0</div>
+    </div>
+
+    <div class="line">
+      <div style="background-color: #f00000">f00000</div>
+      <div style="background-color: #f00040">f00040</div>
+      <div style="background-color: #f00080">f00080</div>
+      <div style="background-color: #f000c0">f000c0</div>
+      <div style="background-color: #f000ff">f000ff</div>
+      <div style="background-color: #f04000">f04000</div>
+      <div style="background-color: #f04040">f04040</div>
+      <div style="background-color: #f04080">f04080</div>
+      <div style="background-color: #f040c0">f040c0</div>
+      <div style="background-color: #f040f0">f040f0</div>
+      <div style="background-color: #f08000">f08000</div>
+      <div style="background-color: #f08040">f08040</div>
+      <div style="background-color: #f08080">f08080</div>
+      <div style="background-color: #f080c0">f080c0</div>
+      <div style="background-color: #f080f0">f080f0</div>
+      <div style="background-color: #f0c000">f0c000</div>
+      <div style="background-color: #f0c040">f0c040</div>
+      <div style="background-color: #f0c080">f0c080</div>
+      <div style="background-color: #f0c0c0">f0c0c0</div>
+      <div style="background-color: #f0c0f0">f0c0f0</div>
+      <div style="background-color: #f0f000">f0f000</div>
+      <div style="background-color: #f0f040">f0f040</div>
+      <div style="background-color: #f0f080">f0f080</div>
+      <div style="background-color: #f0f0c0">f0f0c0</div>
+      <div style="background-color: #f0f0f0">f0f0f0</div>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
+  // name: 'HelloWorld',
   props: {
-    msg: String
-  }
-}
+    msg: String,
+  },
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
+.line {
+  width: 1500px !important;
+  height: 100px !important;
+  display: block;
+  border: none !important;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
+
+.line div {
+  /* color: red; */
+  font-size: 13px;
+  line-height: 50px;
+  vertical-align: middle;
 }
-li {
+
+.red {
+  color: red;
+}
+
+.hello div {
+  width: 50px;
+  height: 50px;
+  /* background-color: red; */
   display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
+  border: 1px solid black;
 }
 </style>
