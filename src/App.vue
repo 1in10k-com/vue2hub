143346157fe5ac3c753d111ca345d98437095f94
playdog-io <cqdxcs3@163.com>
2022-01-28 09:17:35 +0800

    playdog.io OS

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,6 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld/>
   </div>
 </template>
 
@@ -12,17 +11,28 @@ export default {
   name: 'App',
   components: {
     HelloWorld
-  }
+  },
 }
 </script>
 
 <style>
-#app {
+/* #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+} */
+
+
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  /* margin-top: 60px; */
+  margin: 0px;
 }
 </style>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,425 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+    <img :src="arr1[0]" alt="" />
+    <img :src="arr1[1]" alt="" />
+    <img :src="arr1[2]" alt="" />
+    <img :src="arr1[3]" alt="" />
+    <img :src="arr1[4]" alt="" />
+    <img :src="arr1[5]" alt="" />
+    <img :src="arr1[6]" alt="" />
+    <img :src="arr1[7]" alt="" />
+    <img :src="arr1[8]" alt="" />
+    <img :src="arr1[9]" alt="" />
+    <img :src="arr1[10]" alt="" />
+    <img :src="arr1[11]" alt="" />
+    <img :src="arr1[12]" alt="" />
+    <img :src="arr1[13]" alt="" />
+    <img :src="arr1[14]" alt="" />
+    <img :src="arr1[15]" alt="" />
+    <img :src="arr1[16]" alt="" />
+    <img :src="arr1[17]" alt="" />
+    <img :src="arr1[18]" alt="" />
+    <img :src="arr1[19]" alt="" />
+    <img :src="arr1[20]" alt="" />
+    <img :src="arr1[21]" alt="" />
+    <img :src="arr1[22]" alt="" />
+    <img :src="arr1[23]" alt="" />
+    <img :src="arr1[24]" alt="" />
+    <img :src="arr1[25]" alt="" />
+    <img :src="arr1[26]" alt="" />
+    <img :src="arr1[27]" alt="" />
+    <img :src="arr1[28]" alt="" />
+    <img :src="arr1[29]" alt="" />
+    <img :src="arr1[30]" alt="" />
+    <img :src="arr1[31]" alt="" />
+    <img :src="arr1[32]" alt="" />
+    <img :src="arr1[33]" alt="" />
+    <img :src="arr1[34]" alt="" />
+    <img :src="arr1[35]" alt="" />
+    <img :src="arr1[36]" alt="" />
+    <img :src="arr1[37]" alt="" />
+    <img :src="arr1[38]" alt="" />
+    <img :src="arr1[39]" alt="" />
+    <img :src="arr1[40]" alt="" />
+    <img :src="arr1[41]" alt="" />
+    <img :src="arr1[42]" alt="" />
+    <img :src="arr1[43]" alt="" />
+    <img :src="arr1[44]" alt="" />
+    <img :src="arr1[45]" alt="" />
+    <img :src="arr1[46]" alt="" />
+    <img :src="arr1[47]" alt="" />
+    <img :src="arr1[48]" alt="" />
+    <img :src="arr1[49]" alt="" />
+    <img :src="arr1[50]" alt="" />
+    <img :src="arr1[51]" alt="" />
+    <img :src="arr1[52]" alt="" />
+    <img :src="arr1[53]" alt="" />
+    <img :src="arr1[54]" alt="" />
+    <img :src="arr1[55]" alt="" />
+    <img :src="arr1[56]" alt="" />
+    <img :src="arr1[57]" alt="" />
+    <img :src="arr1[58]" alt="" />
+    <img :src="arr1[59]" alt="" />
+    <div class="middle">
+      <div class="words">Playdog is a doggie who loves to play,</div>
+      <div class="words">Do you like him?</div>
+      <div class="words">
+        You can buy it here =>
+        <img @click="toOpensea()" src="opensea.svg" alt="" />
+      </div>
+    </div>
+    <img :src="arr1[60]" alt="" />
+    <img :src="arr1[61]" alt="" />
+    <img :src="arr1[62]" alt="" />
+    <img :src="arr1[63]" alt="" />
+    <img :src="arr1[64]" alt="" />
+    <img :src="arr1[65]" alt="" />
+    <img :src="arr1[66]" alt="" />
+    <img :src="arr1[67]" alt="" />
+    <img :src="arr1[68]" alt="" />
+    <img :src="arr1[69]" alt="" />
+    <img :src="arr1[70]" alt="" />
+    <img :src="arr1[71]" alt="" />
+    <img :src="arr1[72]" alt="" />
+    <img :src="arr1[73]" alt="" />
+    <img :src="arr1[74]" alt="" />
+    <img :src="arr1[75]" alt="" />
+    <img :src="arr1[76]" alt="" />
+    <img :src="arr1[77]" alt="" />
+    <img :src="arr1[78]" alt="" />
+    <img :src="arr1[79]" alt="" />
+    <img :src="arr1[80]" alt="" />
+    <img :src="arr1[81]" alt="" />
+    <img :src="arr1[82]" alt="" />
+    <img :src="arr1[83]" alt="" />
+    <img :src="arr1[84]" alt="" />
+    <img :src="arr1[85]" alt="" />
+    <img :src="arr1[86]" alt="" />
+    <img :src="arr1[87]" alt="" />
+    <img :src="arr1[88]" alt="" />
+    <img :src="arr1[89]" alt="" />
+    <img :src="arr1[90]" alt="" />
+    <img :src="arr1[91]" alt="" />
+    <img :src="arr1[92]" alt="" />
+    <img :src="arr1[93]" alt="" />
+    <img :src="arr1[94]" alt="" />
+    <img :src="arr1[95]" alt="" />
+    <img :src="arr1[96]" alt="" />
+    <img :src="arr1[97]" alt="" />
+    <img :src="arr1[98]" alt="" />
+    <img :src="arr1[99]" alt="" />
+    <img :src="arr1[100]" alt="" />
+    <img :src="arr1[101]" alt="" />
+    <img :src="arr1[102]" alt="" />
+    <img :src="arr1[103]" alt="" />
+    <img :src="arr1[104]" alt="" />
+    <img :src="arr1[105]" alt="" />
+    <img :src="arr1[106]" alt="" />
+    <img :src="arr1[107]" alt="" />
+    <img :src="arr1[108]" alt="" />
+    <img :src="arr1[109]" alt="" />
+    <img :src="arr1[110]" alt="" />
+    <img :src="arr1[111]" alt="" />
+    <img :src="arr1[112]" alt="" />
+    <img :src="arr1[113]" alt="" />
+    <img :src="arr1[114]" alt="" />
+    <img :src="arr1[115]" alt="" />
+    <img :src="arr1[116]" alt="" />
+    <img :src="arr1[117]" alt="" />
+    <img :src="arr1[118]" alt="" />
+    <img :src="arr1[119]" alt="" />
   </div>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
-}
+  name: "HelloWorld",
+  data() {
+    return {
+      arr1: [
+        "001.png",
+        "002.png",
+        "003.png",
+        "004.png",
+        "005.png",
+        "006.png",
+        "007.png",
+        "008.png",
+        "009.png",
+        "010.png",
+        "011.png",
+        "012.png",
+        "013.png",
+        "014.png",
+        "015.png",
+        "016.png",
+        "017.png",
+        "018.png",
+        "019.png",
+        "020.png",
+        "021.png",
+        "022.png",
+        "023.png",
+        "024.png",
+        "025.png",
+        "026.png",
+        "027.png",
+        "028.png",
+        "029.png",
+        "030.png",
+        "031.png",
+        "032.png",
+        "033.png",
+        "034.png",
+        "035.png",
+        "036.png",
+        "037.png",
+        "038.png",
+        "039.png",
+        "040.png",
+        "041.png",
+        "042.png",
+        "043.png",
+        "044.png",
+        "045.png",
+        "046.png",
+        "047.png",
+        "048.png",
+        "049.png",
+        "050.png",
+        "051.png",
+        "052.png",
+        "053.png",
+        "054.png",
+        "055.png",
+        "056.png",
+        "057.png",
+        "058.png",
+        "059.png",
+        "060.png",
+        "061.png",
+        "062.png",
+        "063.png",
+        "064.png",
+        "065.png",
+        "066.png",
+        "067.png",
+        "068.png",
+        "069.png",
+        "070.png",
+        "071.png",
+        "072.png",
+        "073.png",
+        "074.png",
+        "075.png",
+        "076.png",
+        "077.png",
+        "078.png",
+        "079.png",
+        "080.png",
+        "081.png",
+        "082.png",
+        "083.png",
+        "084.png",
+        "085.png",
+        "086.png",
+        "087.png",
+        "088.png",
+        "089.png",
+        "090.png",
+        "091.png",
+        "092.png",
+        "093.png",
+        "094.png",
+        "095.png",
+        "096.png",
+        "097.png",
+        "098.png",
+        "099.png",
+        "100.png",
+        "101.png",
+        "102.png",
+        "103.png",
+        "104.png",
+        "105.png",
+        "106.png",
+        "107.png",
+        "108.png",
+        "109.png",
+        "110.png",
+        "111.png",
+        "112.png",
+        "113.png",
+        "114.png",
+        "115.png",
+        "116.png",
+        "117.png",
+        "118.png",
+        "119.png",
+        "120.png",
+        "121.png",
+        "122.png",
+        "123.png",
+        "124.png",
+        "125.png",
+      ],
+      arr2: [
+        "001.png",
+        "002.png",
+        "003.png",
+        "004.png",
+        "005.png",
+        "006.png",
+        "007.png",
+        "008.png",
+        "009.png",
+        "010.png",
+        "011.png",
+        "012.png",
+        "013.png",
+        "014.png",
+        "015.png",
+        "016.png",
+        "017.png",
+        "018.png",
+        "019.png",
+        "020.png",
+        "021.png",
+        "022.png",
+        "023.png",
+        "024.png",
+        "025.png",
+        "026.png",
+        "027.png",
+        "028.png",
+        "029.png",
+        "030.png",
+        "031.png",
+        "032.png",
+        "033.png",
+        "034.png",
+        "035.png",
+        "036.png",
+        "037.png",
+        "038.png",
+        "039.png",
+        "040.png",
+        "041.png",
+        "042.png",
+        "043.png",
+        "044.png",
+        "045.png",
+        "046.png",
+        "047.png",
+        "048.png",
+        "049.png",
+        "050.png",
+        "051.png",
+        "052.png",
+        "053.png",
+        "054.png",
+        "055.png",
+        "056.png",
+        "057.png",
+        "058.png",
+        "059.png",
+        "060.png",
+        "061.png",
+        "062.png",
+        "063.png",
+        "064.png",
+        "065.png",
+        "066.png",
+        "067.png",
+        "068.png",
+        "069.png",
+        "070.png",
+        "071.png",
+        "072.png",
+        "073.png",
+        "074.png",
+        "075.png",
+        "076.png",
+        "077.png",
+        "078.png",
+        "079.png",
+        "080.png",
+        "081.png",
+        "082.png",
+        "083.png",
+        "084.png",
+        "085.png",
+        "086.png",
+        "087.png",
+        "088.png",
+        "089.png",
+        "090.png",
+        "091.png",
+        "092.png",
+        "093.png",
+        "094.png",
+        "095.png",
+        "096.png",
+        "097.png",
+        "098.png",
+        "099.png",
+        "100.png",
+        "101.png",
+        "102.png",
+        "103.png",
+        "104.png",
+        "105.png",
+        "106.png",
+        "107.png",
+        "108.png",
+        "109.png",
+        "110.png",
+        "111.png",
+        "112.png",
+        "113.png",
+        "114.png",
+        "115.png",
+        "116.png",
+        "117.png",
+        "118.png",
+        "119.png",
+        "120.png",
+        "121.png",
+        "122.png",
+        "123.png",
+        "124.png",
+        "125.png",
+      ],
+    };
+  },
+  methods: {
+    toOpensea() {
+      window.location.href =
+        "https://opensea.io/collection/playdog-pure?search[sortAscending]=true&search[sortBy]=CREATED_DATE";
+    },
+  },
+  created() {
+    for (let i = 0; i < this.arr2.length; i++) {
+      var ram = Math.floor(Math.random() * this.arr2.length);
+      this.arr1[i] = this.arr2[ram];
+      this.arr2.splice(ram, 1);
+    }
+  },
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
+.hello img {
+  width: 5%;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
+.middle {
+  height: 200px;
+  background-color: pink;
+  padding: 10px;
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
+
+.words {
+  margin-top: 20px;
 }
 </style>
