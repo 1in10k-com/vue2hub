75411996678b6db153ab42bd3111127e1cd11601
playdog-io <cqdxcs3@163.com>
2022-01-28 14:25:06 +0800

    deleted color words in div

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,143 +1,143 @@
 <template>
   <div class="hello">
     <div class="line red">
-      <div style="background-color: #000000">000000</div>
-      <div style="background-color: #000040">000040</div>
-      <div style="background-color: #000080">000080</div>
-      <div style="background-color: #0000c0">0000c0</div>
-      <div style="background-color: #0000ff">0000ff</div>
-      <div style="background-color: #004000">004000</div>
-      <div style="background-color: #004040">004040</div>
-      <div style="background-color: #004080">004080</div>
-      <div style="background-color: #0040c0">0040c0</div>
-      <div style="background-color: #0040ff">0040ff</div>
-      <div style="background-color: #008000">008000</div>
-      <div style="background-color: #008040">008040</div>
-      <div style="background-color: #008080">008080</div>
-      <div style="background-color: #0080c0">0080c0</div>
-      <div style="background-color: #0080ff">0080ff</div>
-      <div style="background-color: #00c000">00c000</div>
-      <div style="background-color: #00c040">00c040</div>
-      <div style="background-color: #00c080">00c080</div>
-      <div style="background-color: #00c0c0">00c0c0</div>
-      <div style="background-color: #00c0ff">00c0ff</div>
-      <div style="background-color: #00ff00">00ff00</div>
-      <div style="background-color: #00ff40">00ff40</div>
-      <div style="background-color: #00ff80">00ff80</div>
-      <div style="background-color: #00ffc0">00ffc0</div>
-      <div style="background-color: #00ffff">00ffff</div>
+      <div style="background-color: #000000"></div>
+      <div style="background-color: #000040"></div>
+      <div style="background-color: #000080"></div>
+      <div style="background-color: #0000c0"></div>
+      <div style="background-color: #0000ff"></div>
+      <div style="background-color: #004000"></div>
+      <div style="background-color: #004040"></div>
+      <div style="background-color: #004080"></div>
+      <div style="background-color: #0040c0"></div>
+      <div style="background-color: #0040ff"></div>
+      <div style="background-color: #008000"></div>
+      <div style="background-color: #008040"></div>
+      <div style="background-color: #008080"></div>
+      <div style="background-color: #0080c0"></div>
+      <div style="background-color: #0080ff"></div>
+      <div style="background-color: #00c000"></div>
+      <div style="background-color: #00c040"></div>
+      <div style="background-color: #00c080"></div>
+      <div style="background-color: #00c0c0"></div>
+      <div style="background-color: #00c0ff"></div>
+      <div style="background-color: #00ff00"></div>
+      <div style="background-color: #00ff40"></div>
+      <div style="background-color: #00ff80"></div>
+      <div style="background-color: #00ffc0"></div>
+      <div style="background-color: #00ffff"></div>
     </div>
 
     <div class="line red">
-      <div style="background-color: #400000">400000</div>
-      <div style="background-color: #400040">400040</div>
-      <div style="background-color: #400080">400080</div>
-      <div style="background-color: #4000c0">4000c0</div>
-      <div style="background-color: #4000ff">4000ff</div>
-      <div style="background-color: #404000">404000</div>
-      <div style="background-color: #404040">404040</div>
-      <div style="background-color: #404080">404080</div>
-      <div style="background-color: #4040c0">4040c0</div>
-      <div style="background-color: #4040ff">4040ff</div>
-      <div style="background-color: #408000">408000</div>
-      <div style="background-color: #408040">408040</div>
-      <div style="background-color: #408080">408080</div>
-      <div style="background-color: #4080c0">4080c0</div>
-      <div style="background-color: #4080ff">4080ff</div>
-      <div style="background-color: #40c000">40c000</div>
-      <div style="background-color: #40c040">40c040</div>
-      <div style="background-color: #40c080">40c080</div>
-      <div style="background-color: #40c0c0">40c0c0</div>
-      <div style="background-color: #40c0ff">40c0ff</div>
-      <div style="background-color: #40ff00">40ff00</div>
-      <div style="background-color: #40ff40">40ff40</div>
-      <div style="background-color: #40ff80">40ff80</div>
-      <div style="background-color: #40ffc0">40ffc0</div>
-      <div style="background-color: #40ffff">40ffff</div>
+      <div style="background-color: #400000"></div>
+      <div style="background-color: #400040"></div>
+      <div style="background-color: #400080"></div>
+      <div style="background-color: #4000c0"></div>
+      <div style="background-color: #4000ff"></div>
+      <div style="background-color: #404000"></div>
+      <div style="background-color: #404040"></div>
+      <div style="background-color: #404080"></div>
+      <div style="background-color: #4040c0"></div>
+      <div style="background-color: #4040ff"></div>
+      <div style="background-color: #408000"></div>
+      <div style="background-color: #408040"></div>
+      <div style="background-color: #408080"></div>
+      <div style="background-color: #4080c0"></div>
+      <div style="background-color: #4080ff"></div>
+      <div style="background-color: #40c000"></div>
+      <div style="background-color: #40c040"></div>
+      <div style="background-color: #40c080"></div>
+      <div style="background-color: #40c0c0"></div>
+      <div style="background-color: #40c0ff"></div>
+      <div style="background-color: #40ff00"></div>
+      <div style="background-color: #40ff40"></div>
+      <div style="background-color: #40ff80"></div>
+      <div style="background-color: #40ffc0"></div>
+      <div style="background-color: #40ffff"></div>
     </div>
 
     <div class="line red">
-      <div style="background-color: #800000">800000</div>
-      <div style="background-color: #800040">800040</div>
-      <div style="background-color: #800080">800080</div>
-      <div style="background-color: #8000c0">8000c0</div>
-      <div style="background-color: #8000ff">8000ff</div>
-      <div style="background-color: #804000">804000</div>
-      <div style="background-color: #804040">804040</div>
-      <div style="background-color: #804080">804080</div>
-      <div style="background-color: #8040c0">8040c0</div>
-      <div style="background-color: #8040ff">8040ff</div>
-      <div style="background-color: #808000">808000</div>
-      <div style="background-color: #808040">808040</div>
-      <div style="background-color: #808080">808080</div>
-      <div style="background-color: #8080c0">8080c0</div>
-      <div style="background-color: #8080ff">8080ff</div>
-      <div style="background-color: #80c000">80c000</div>
-      <div style="background-color: #80c040">80c040</div>
-      <div style="background-color: #80c080">80c080</div>
-      <div style="background-color: #80c0c0">80c0c0</div>
-      <div style="background-color: #80c0ff">80c0ff</div>
-      <div style="background-color: #80ff00">80ff00</div>
-      <div style="background-color: #80ff40">80ff40</div>
-      <div style="background-color: #80ff80">80ff80</div>
-      <div style="background-color: #80ffc0">80ffc0</div>
-      <div style="background-color: #80ffff">80ffff</div>
+      <div style="background-color: #800000"></div>
+      <div style="background-color: #800040"></div>
+      <div style="background-color: #800080"></div>
+      <div style="background-color: #8000c0"></div>
+      <div style="background-color: #8000ff"></div>
+      <div style="background-color: #804000"></div>
+      <div style="background-color: #804040"></div>
+      <div style="background-color: #804080"></div>
+      <div style="background-color: #8040c0"></div>
+      <div style="background-color: #8040ff"></div>
+      <div style="background-color: #808000"></div>
+      <div style="background-color: #808040"></div>
+      <div style="background-color: #808080"></div>
+      <div style="background-color: #8080c0"></div>
+      <div style="background-color: #8080ff"></div>
+      <div style="background-color: #80c000"></div>
+      <div style="background-color: #80c040"></div>
+      <div style="background-color: #80c080"></div>
+      <div style="background-color: #80c0c0"></div>
+      <div style="background-color: #80c0ff"></div>
+      <div style="background-color: #80ff00"></div>
+      <div style="background-color: #80ff40"></div>
+      <div style="background-color: #80ff80"></div>
+      <div style="background-color: #80ffc0"></div>
+      <div style="background-color: #80ffff"></div>
     </div>
 
     <div class="line">
-      <div style="background-color: #c00000">c00000</div>
-      <div style="background-color: #c00040">c00040</div>
-      <div style="background-color: #c00080">c00080</div>
-      <div style="background-color: #c000c0">c000c0</div>
-      <div style="background-color: #c000ff">c000ff</div>
-      <div style="background-color: #c04000">c04000</div>
-      <div style="background-color: #c04040">c04040</div>
-      <div style="background-color: #c04080">c04080</div>
-      <div style="background-color: #c040c0">c040c0</div>
-      <div style="background-color: #c040ff">c040ff</div>
-      <div style="background-color: #c08000">c08000</div>
-      <div style="background-color: #c08040">c08040</div>
-      <div style="background-color: #c08080">c08080</div>
-      <div style="background-color: #c080c0">c080c0</div>
-      <div style="background-color: #c080ff">c080ff</div>
-      <div style="background-color: #c0c000">c0c000</div>
-      <div style="background-color: #c0c040">c0c040</div>
-      <div style="background-color: #c0c080">c0c080</div>
-      <div style="background-color: #c0c0c0">c0c0c0</div>
-      <div style="background-color: #c0c0ff">c0c0ff</div>
-      <div style="background-color: #c0ff00">c0ff00</div>
-      <div style="background-color: #c0ff40">c0ff40</div>
-      <div style="background-color: #c0ff80">c0ff80</div>
-      <div style="background-color: #c0ffc0">c0ffc0</div>
-      <div style="background-color: #c0ffff">c0ffff</div>
+      <div style="background-color: #c00000"></div>
+      <div style="background-color: #c00040"></div>
+      <div style="background-color: #c00080"></div>
+      <div style="background-color: #c000c0"></div>
+      <div style="background-color: #c000ff"></div>
+      <div style="background-color: #c04000"></div>
+      <div style="background-color: #c04040"></div>
+      <div style="background-color: #c04080"></div>
+      <div style="background-color: #c040c0"></div>
+      <div style="background-color: #c040ff"></div>
+      <div style="background-color: #c08000"></div>
+      <div style="background-color: #c08040"></div>
+      <div style="background-color: #c08080"></div>
+      <div style="background-color: #c080c0"></div>
+      <div style="background-color: #c080ff"></div>
+      <div style="background-color: #c0c000"></div>
+      <div style="background-color: #c0c040"></div>
+      <div style="background-color: #c0c080"></div>
+      <div style="background-color: #c0c0c0"></div>
+      <div style="background-color: #c0c0ff"></div>
+      <div style="background-color: #c0ff00"></div>
+      <div style="background-color: #c0ff40"></div>
+      <div style="background-color: #c0ff80"></div>
+      <div style="background-color: #c0ffc0"></div>
+      <div style="background-color: #c0ffff"></div>
     </div>
 
     <div class="line">
-      <div style="background-color: #ff0000">ff0000</div>
-      <div style="background-color: #ff0040">ff0040</div>
-      <div style="background-color: #ff0080">ff0080</div>
-      <div style="background-color: #ff00c0">ff00c0</div>
-      <div style="background-color: #ff00ff">ff00ff</div>
-      <div style="background-color: #ff4000">ff4000</div>
-      <div style="background-color: #ff4040">ff4040</div>
-      <div style="background-color: #ff4080">ff4080</div>
-      <div style="background-color: #ff40c0">ff40c0</div>
-      <div style="background-color: #ff40ff">ff40ff</div>
-      <div style="background-color: #ff8000">ff8000</div>
-      <div style="background-color: #ff8040">ff8040</div>
-      <div style="background-color: #ff8080">ff8080</div>
-      <div style="background-color: #ff80c0">ff80c0</div>
-      <div style="background-color: #ff80ff">ff80ff</div>
-      <div style="background-color: #ffc000">ffc000</div>
-      <div style="background-color: #ffc040">ffc040</div>
-      <div style="background-color: #ffc080">ffc080</div>
-      <div style="background-color: #ffc0c0">ffc0c0</div>
-      <div style="background-color: #ffc0ff">ffc0ff</div>
-      <div style="background-color: #ffff00">ffff00</div>
-      <div style="background-color: #ffff40">ffff40</div>
-      <div style="background-color: #ffff80">ffff80</div>
-      <div style="background-color: #ffffc0">ffffc0</div>
-      <div style="background-color: #ffffff">ffffff</div>
+      <div style="background-color: #ff0000"></div>
+      <div style="background-color: #ff0040"></div>
+      <div style="background-color: #ff0080"></div>
+      <div style="background-color: #ff00c0"></div>
+      <div style="background-color: #ff00ff"></div>
+      <div style="background-color: #ff4000"></div>
+      <div style="background-color: #ff4040"></div>
+      <div style="background-color: #ff4080"></div>
+      <div style="background-color: #ff40c0"></div>
+      <div style="background-color: #ff40ff"></div>
+      <div style="background-color: #ff8000"></div>
+      <div style="background-color: #ff8040"></div>
+      <div style="background-color: #ff8080"></div>
+      <div style="background-color: #ff80c0"></div>
+      <div style="background-color: #ff80ff"></div>
+      <div style="background-color: #ffc000"></div>
+      <div style="background-color: #ffc040"></div>
+      <div style="background-color: #ffc080"></div>
+      <div style="background-color: #ffc0c0"></div>
+      <div style="background-color: #ffc0ff"></div>
+      <div style="background-color: #ffff00"></div>
+      <div style="background-color: #ffff40"></div>
+      <div style="background-color: #ffff80"></div>
+      <div style="background-color: #ffffc0"></div>
+      <div style="background-color: #ffffff"></div>
     </div>
   </div>
 </template>
@@ -155,7 +155,7 @@ export default {
 <style scoped>
 .line {
   width: 1500px !important;
-  height: 100px !important;
+  height: 50px !important;
   display: block;
   border: none !important;
 }
@@ -176,6 +176,6 @@ export default {
   height: 50px;
   /* background-color: red; */
   display: inline-block;
-  border: 1px solid black;
+  /* border: 1px solid black; */
 }
 </style>
